89ea06a37894655e02087e9c7bdc07dd9e788356
Fondolski <fondolski@gmail.com>
2025-04-27 00:56:33 +0300

    ignored backend node modules

--- src/app/components/HomeHeroComponent.tsx
+++ src/app/components/HomeHeroComponent.tsx
@@ -29,7 +29,7 @@ export default function HomeHeroComponent({heroImages}: {heroImages: string []})
             <div className='w-full h-full backdrop-blur-xs  flex items-center justify-center flex-col mt-5'>
                 <div className='flex flex-col items-center'>
                     <div className='text-xl font-extrabold bg-gradient-to-r from-cyan-400 to-pink-500 px-2 mr-20'>{`LET'S GO`}</div>
-                    <div className='text-3xl font-extrabold bg-gradient-to-r from-green-400 to-blue-500 px-2 mr-32'>FIND YOR WAY</div>
+                    <div className='text-3xl font-extrabold bg-gradient-to-r from-green-400 to-blue-500 px-2 mr-32'>FIND YOUR WAY</div>
                     <div className='text-3xl font-extrabold px-2 bg-gradient-to-r from-purple-400 to-white ml-20'>TO ANY SPECIAL PLACE</div>
                 </div>
                 <div className='flex flex-row items-center justify-between mt-5 w-11/12  max-w-2xl'>
