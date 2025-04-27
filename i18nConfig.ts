5bd77ca6b77ab4d2f0d9558f932a8566808c5472
Fondolski <fondolski@gmail.com>
2025-04-27 21:21:06 +0300

    changed i18nConfig

--- i18nConfig.ts
+++ i18nConfig.ts
@@ -1,6 +1,8 @@
-module.exports = {
-    i18n: {
-      defaultLocale: 'en',
-      locales: ['en', 'fr'],
-    },
-  };
+const i18nConfig = {
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en', 'fr'],
+  },
+};
+
+export default i18nConfig;
